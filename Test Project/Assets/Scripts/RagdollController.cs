d62ec1fcf93e9a87a0fdd1243b94b8188b9b8430
Alex C <alcardaras@gmail.com>
2019-04-21 00:50:39 -0700

    Ragdoll

---
+++ Test Project/Assets/Scripts/RagdollController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollController : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody[] rbs;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        toggleRagdoll(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Ragdoll");
+            toggleRagdoll(true);
+        }
+    }
+
+    private void toggleRagdoll(bool isRagdoll)
+    {
+        anim.enabled = !isRagdoll;
+        foreach (var item in rbs)
+        {
+            item.isKinematic = !isRagdoll;
+        }
+    }
+}
